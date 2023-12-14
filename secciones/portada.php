5543f25b8b289f1d499493f62d8ac862a25ec88c
Emilio Cara Garzón <57097188+ecg578@users.noreply.github.com>
2023-12-14 16:59:37 +0100

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "La portada CC2022",
-"autor" => "mtorres",
-"resumen" => "El resumen maravilloso",
+"titulo" => "Portada actualizada",
+"autor" => "ecg578",
+"resumen" => "Resumen de la portada",
 ];
 ?>
